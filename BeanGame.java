8a8b1a86f2668066b9e007ae1e44cb518670ef62
andyyoyo <andyyoyo76543@gmail.com>
2016-03-22 22:59:52 +0800

    add the buttom lines

--- BeanGame.java
+++ BeanGame.java
@@ -17,10 +17,10 @@ public class BeanGame extends Application{
 		Line upl1 = new Line(175,40,175,50);
 		Line upr1 = new Line(225,40,225,50);
 		Line ll = new Line(175,50,105,210);
-		Line rl = new Line(225,50,345,210);
+		Line rl = new Line(225,50,305,210);
 		Line dl = new Line(105,210,105,240);
-		Line dr = new Line(345,210,345,240);
-		Line d = new Line(105,240,345,240);
+		Line dr = new Line(305,210,305,240);
+		Line d = new Line(105,240,305,240);
 		
 		p.getChildren().add(upl1);
 		p.getChildren().add(upr1);
@@ -30,3 +30,21 @@ public class BeanGame extends Application{
 		p.getChildren().add(dr);
 		p.getChildren().add(d);
 		
+		Line[] line = new Line[8];
+		for(int i =1;i<=7;i++){
+			
+			line [i] = new Line(105+25*i,210,105+25*i,240);
+			p.getChildren().add(line[i]);
+		}
+		
+		Scene scene = new Scene(p);
+		primaryStage.setTitle("Bean game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main (String[] args){
+		launch(args);
+	}
+	
+}
+
